e749abbb3251a1524658ce618501a07dc6ae48fe
Peter Vuyk <peter.vuyk@topicus.nl>
2021-06-06 15:25:37 +0200

    Only collect published (not draft) regulations from firebase

--- src/database/firebase/collectRegulations.ts
+++ src/database/firebase/collectRegulations.ts
@@ -2,7 +2,10 @@ import database from './database';
 import { Regulation } from '../repository/regulationRepository';
 
 async function getRegulations(): Promise<Regulation[]> {
-  const querySnapshot = await database().collection('regulations').get();
+  const querySnapshot = await database()
+    .collection('regulations')
+    .where('isDraft', '==', false)
+    .get();
   return querySnapshot.docs.map(doc => doc.data() as Regulation);
 }
 
