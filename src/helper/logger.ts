c4bfa67c7194dec67644806ce6937e73aafe9347
Peter Vuyk <peter.vuyk@topicus.nl>
2021-07-11 12:36:27 +0200

    log local logging to console, production log still to bugsnag

--- src/helper/logger.ts
+++ src/helper/logger.ts
@@ -1,12 +1,20 @@
 import Bugsnag from '@bugsnag/expo';
 
 const error = (errorMessage: string, reason: string) => {
+  if (__DEV__) {
+    console.error('errorMessage', errorMessage, 'reason', reason);
+    return;
+  }
   Bugsnag.notify(
     new Error(`errorMessage: ${errorMessage} error reason: ${reason}`),
   );
 };
 
 const errorFromMessage = (errorMessage: string) => {
+  if (__DEV__) {
+    console.error('errorMessage', errorMessage);
+    return;
+  }
   Bugsnag.notify(new Error(`errorMessage: ${errorMessage}`));
 };
 
