abf76258bee81ee65a18592d47649199def75082
Peter Vuyk <peter.vuyk@topicus.nl>
2021-10-16 07:45:09 +0200

    Removed test data about us with lorem ipsum

--- src/screens/about/AboutUsScreen.tsx
+++ src/screens/about/AboutUsScreen.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { Image, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { DrawerNavigationProp } from '@react-navigation/drawer/lib/typescript/src/types';
 import TitleBar from '../../components/TitleBar';
 
@@ -20,26 +20,13 @@ const AboutUsScreen: FC<Props> = ({ navigation }) => {
     <View style={styles.container}>
       <TitleBar
         title="Wie zijn wij?"
-        subTitle="Graag willen we ons even aan je voorstellen, wij zijn Eddie en Peter. Ik
-        (Eddie) ben al 87 jaar ambulance broeder en daarvoor militair. En ik
-        (Peter) ben 5 jaar software ontwikkelaar. Beide zijn wij erg enthousiast
-        dat je gebruik maakt van deze app... Neque porro quisquam est qui
-        dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+        subTitle="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur,
+         adipisci velit. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur,
+          adipisci velit. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur,
+           adipisci velit. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur,
+            adipisci velit. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur,
+             adipisci velit. "
       />
-      <View
-        style={{
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Image
-          style={{
-            resizeMode: 'contain',
-          }}
-          source={require('../../../assets/images/donkeys.png')}
-        />
-      </View>
       <TitleBar
         title="Vragen? Ideeën? Neem contact op!"
         subTitle="Stuur ons een email naar: email@email.nl"
