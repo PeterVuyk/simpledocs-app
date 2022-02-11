3db9227064cf59182128694e9b45a729e399f3a4
Peter Vuyk <peter.vuyk@topicus.nl>
2022-02-11 16:32:32 +0100

    Disable temporary dimensions reload for detail pages because it interferes with showing video's in full screen landscape mode

--- src/components/viewer/DimensionsProvider.tsx
+++ src/components/viewer/DimensionsProvider.tsx
@@ -1,35 +1,42 @@
-import React, { FC, ReactNode, useCallback, useEffect, useState } from 'react';
+import React, { FC, ReactNode } from 'react';
 import { Dimensions, ScaledSize } from 'react-native';
 
 interface Props {
   children: (window: ScaledSize) => ReactNode;
 }
 
+/**
+ * The goal was to recalculate the dimensions for users with a flip phone,
+ * or for the future when we support landscape apps. The problem is that
+ * the dimensions event listener gets triggered when a user watch a full screen video.
+ * For now we comment that functionality and have to look later for a workaround.
+ */
 const DimensionsProvider: FC<Props> = ({ children }) => {
   const window = Dimensions.get('window');
-  const [dimensions, setDimensions] = useState({ window });
-  const [loading, setLoading] = useState<boolean>(true);
+  // const [dimensions, setDimensions] = useState({ window });
+  // const [loading, setLoading] = useState<boolean>(true);
 
-  const onChange = useCallback(({ window }) => {
-    setLoading(true);
-    setDimensions({ window });
-    setLoading(false);
-  }, []);
+  // const onChange = useCallback(({ window }) => {
+  //   setLoading(true);
+  //   setDimensions({ window });
+  //   setLoading(false);
+  // }, []);
 
-  useEffect(() => {
-    setLoading(true);
-    setDimensions({ window });
-    setLoading(false);
-  }, [onChange, window]);
+  // useEffect(() => {
+  //   setLoading(true);
+  //   setDimensions({ window });
+  //   setLoading(false);
+  // }, [onChange, window]);
 
-  useEffect(() => {
-    Dimensions.addEventListener('change', onChange);
-    return () => {
-      Dimensions.removeEventListener('change', onChange);
-    };
-  });
+  // useEffect(() => {
+  //   Dimensions.addEventListener('change', onChange);
+  //   return () => {
+  //     Dimensions.removeEventListener('change', onChange);
+  //   };
+  // });
 
-  return <>{!loading && children(dimensions.window)}</>;
+  return <>{children(window)}</>;
+  // return <>{!loading && children(dimensions.window)}</>;
 };
 
 export default DimensionsProvider;
